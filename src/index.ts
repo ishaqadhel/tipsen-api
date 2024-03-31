0ec03e1e194efa496474811ff7f5fd0b8fdae064
ishaqadhel <ishaq.adhel@gmail.com>
2024-03-31 13:08:38 +0700

    chore: add eslint and prettier

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,21 @@
-import express, { Application, Request, Response, NextFunction } from 'express';
+import express, {
+  type Application,
+  type Request,
+  type Response,
+  type NextFunction
+} from 'express'
 
 // Boot express
-const app: Application = express();
-const port = 8080;
+const app: Application = express()
+const port = 8080
 
 // Application routing
-app.use('/', (req: Request, res: Response, next: NextFunction ) => {
-    res.status(200).send({data: 'Welcome to Tipsen API.'});
-});
+app.use('/', (req: Request, res: Response, next: NextFunction) => {
+  res.status(200).send({ data: 'Welcome to Tipsen API.' })
+})
 
 // Start server
-app.listen(port, () => console.log(`Server is listening on port ${port}!`));
+app.listen(port, () => {
+  // eslint-disable-next-line no-console
+  console.log(`Server is listening on port ${port}!`)
+})
